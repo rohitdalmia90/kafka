f8718a68ebfdcfcf73469a95721da94916fd4754
rohitdalmia90 <rohitdalmia90@gmail.com>
2020-11-26 23:57:58 +0530

    first commit

---
+++ src/main/java/com/kafka/project/KafkaSimpleController.java
@@ -0,0 +1,10 @@
+package com.kafka.project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaSimpleController {
+
+}
